0fb2c3ec52dd2d936fa3f6b404595272215ad2f9
Arthur <arthur.perronnet@gmail.com>
2024-03-30 07:57:33 +0100

    Corrected the errors from the original file

--- lacs.ml
+++ lacs.ml
@@ -51,6 +51,14 @@ let carte2 = [|
 [|0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;|]
 |];;
 
+let carte3 = [|
+[|0;0;0;0;0|];
+[|0;1;1;1;0|];
+[|0;1;1;1;0|];
+[|0;1;1;1;0|];
+[|0;0;0;0;0|]
+|]
+
 (* affiche un tabelau 2D *)
 let affiche_ligne t = 
   for i = 0 to Array.length t - 1 do
@@ -75,35 +83,41 @@ affiche carte0;;
 Printf.printf "\n\n";;
 
 let existe x y length height =
+  (*Vérifie que les coord fournies existent*)
   x >= 0 && x < length && y >= 0 && y < height
 ;;
 
 let voisins carte x y length height =
-  let l = ref [] in
-  for i = -1 to 1 do
-    for j = -1 to 1 do
-      if existe (x+i) (y+j) length height then l := !l@x+i::y+j::[]
-    done
-  done;
-  !l
+  (*Renvoie une liste contenant les cases se trouvant au nord, à l'est, à l'ouest et au sud de carte.(x).(y)*)
+  let lcoord = 0::-1::-1::0::1::0::0::1::[] in
+  let rec aux l x y length height =
+    match l with
+    | [] -> []
+    | i::j::ll -> if existe (x+i) (y+j) length height then x+i::y+j::aux ll x y length height
+                  else aux ll x y length height
+    | i::ll -> failwith "Liste mal encodée"
+  in
+  aux lcoord x y length height
 ;;
 
 let toucheterre carte x y length height =
+  (*Regarde si carte.(x).(y) se trouve à côté d'une case de terre*)
   let rec aux l =
     match l with
     | [] -> false
     | x::y::ll -> if carte.(x).(y) = 0 then true else aux ll
-    | x::ll -> failwith "liste mal encodée"
+    | x::ll -> failwith "Liste mal encodée"
   in
   let liste = voisins carte x y length height in
   aux liste
 ;;
 
 let rec transfovois carte l =
+  (*Transforme toute les cases de carte de coord se trouvant dans l en case de terre*)
   match l with
   | [] -> ()
   | x::y::ll -> carte.(x).(y) <- 0; transfovois carte ll
-  | x::ll -> failwith "liste mal encodée"
+  | x::ll -> failwith "Liste mal encodée"
 ;;
   
 
@@ -114,7 +128,7 @@ let profondeur carte =
   let l = ref [] in
   let fini = ref false in
   while not !fini do
-    prof := !prof + 1;
+    (*Tant qu'on continue à transformer des cases en case terre, c'est que l'on n'a pas atteint le fond*)
     l := [];
     for i = 0 to (length-1) do
       for j = 0 to (height-1) do
@@ -123,7 +137,8 @@ let profondeur carte =
       done;
     done;
     if !l <> [] then
-      transfovois carte !l
+      (prof := !prof + 1;
+      transfovois carte !l)
     else fini := true
   done;
   !prof
@@ -133,3 +148,4 @@ let profondeur carte =
 Printf.printf "La prof de la carte0 est %d\n" (profondeur carte0);;
 Printf.printf "La prof de la carte1 est %d\n" (profondeur carte1);;
 Printf.printf "La prof de la carte2 est %d\n" (profondeur carte2);;
+Printf.printf "La prof de la carte3 est %d\n" (profondeur carte3);;
